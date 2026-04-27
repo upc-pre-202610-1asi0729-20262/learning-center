52f7c431c4ae879f7590b1c0e74eb8c52bf69ede
Angel Velasquez <pcisavel@upc.edu.pe>
2026-04-27 07:54:28 -0500

    feat(environment): add development environment configuration.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1/',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderCoursesEndpointPath: '/courses',
+  platformProviderSignInEndpointPath: '/authentication/sign-in',
+  platformProviderSignUpEndpointPath: '/authentication/sign-up',
+  logoProviderApiBaseUrl: 'https://img.logo.dev/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1/',
+  platformProviderCategoriesEndpointPath: '/categories',
+  platformProviderCoursesEndpointPath: '/courses',
+  platformProviderSignInEndpointPath: '/authentication/sign-in',
+  platformProviderSignUpEndpointPath: '/authentication/sign-up',
+  logoProviderApiBaseUrl: 'https://img.logo.dev/',
+};
