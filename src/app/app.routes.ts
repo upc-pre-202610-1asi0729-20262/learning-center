f565f86a5dac8851c2f72e9df99d07d1ddd16e34
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-02 23:24:34 -0500

    feat(routes): update routing configuration for public access and layout integration.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { Home } from './shared/presentation/views/home/home';
 
-export const routes: Routes = [];
+const about = () => import('./shared/presentation/views/about/about').then((m) => m.About);
+const pageNotFound = () =>
+  import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
+const learningRoutes = () =>
+  import('./learning/presentation/learning.routes').then((m) => m.learningRoutes);
+/*
+// Uncomment when IAM is implemented
+const iamRoutes = () => import('./iam/presentation/iam.routes').then(m => m.iamRoutes);
+*/
+const baseTitle = 'ACME Learning Center';
+
+/**
+ * Root route configuration that composes bounded-context routes.
+ */
+/*
+// IAM-enabled Routes version to replace when IAM is implemented
+export const routes: Routes = [
+  { path: 'home',     component:      Home,                         title: `${baseTitle} - Home`, canActivate: [iamGuard]  },
+  { path: 'about',    loadComponent:  about,                        title: `${baseTitle} - About`  },
+  { path: 'learning', loadChildren:  learningRoutes, canActivate: [iamGuard]},
+  { path: 'iam',      loadChildren:   iamRoutes },
+  { path: '',         redirectTo:     '/home', pathMatch: 'full'  },
+  { path: '**',       loadComponent:  pageNotFound,                 title: `${baseTitle} - Page Not Found`  },
+];
+*/
+
+// Public Routes version to use until IAM is implemented
+export const routes: Routes = [
+  { path: 'home', component: Home, title: `${baseTitle} - Home` },
+  { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
+  { path: 'learning', loadChildren: learningRoutes },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Layout } from './shared/presentation/components/layout/layout';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('learning-center');
+
+  /**
+   * Translation service instance.
+   */
+  private translate: TranslateService;
+
+  /**
+   * Creates an instance of App and sets up translation.
+   */
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
 }
