4ca5c368ecc2595c3076ef6a8c8a9583be0271bb
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-02 23:19:17 -0500

    feat(api): implement course management methods in learning api.

--- src/app/learning/infrastructure/learning-api.ts
+++ src/app/learning/infrastructure/learning-api.ts
@@ -4,16 +4,63 @@ import { CategoriesApiEndpoint } from './categories-api-endpoint';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../domain/model/category.entity';
 import { Observable } from 'rxjs';
+import { CoursesApiEndpoint } from './courses-api-endpoint';
+import { Course } from '../domain/model/couse.entity';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class LearningApi extends BaseApi {
   private readonly categoriesEndpoint: CategoriesApiEndpoint;
+  private readonly coursesEndpoint: CoursesApiEndpoint;
 
   constructor(http: HttpClient) {
     super();
+    this.coursesEndpoint = new CoursesApiEndpoint(http);
     this.categoriesEndpoint = new CategoriesApiEndpoint(http);
   }
 
+  /**
+   * Retrieves all courses.
+   * @returns Stream with the course collection.
+   */
+  getCourses(): Observable<Course[]> {
+    return this.coursesEndpoint.getAll();
+  }
+
+  /**
+   * Retrieves a single course by ID.
+   * @param id - The ID of the course.
+   * @returns An Observable of the Course object.
+   */
+  getCourse(id: number): Observable<Course> {
+    return this.coursesEndpoint.getById(id);
+  }
+
+  /**
+   * Creates a new course.
+   * @param course - The course to create.
+   * @returns An Observable of the created Course object.
+   */
+  createCourse(course: Course): Observable<Course> {
+    return this.coursesEndpoint.create(course);
+  }
+
+  /**
+   * Updates an existing course.
+   * @param course - The course to update.
+   * @returns An Observable of the updated Course object.
+   */
+  updateCourse(course: Course): Observable<Course> {
+    return this.coursesEndpoint.update(course, course.id);
+  }
+
+  /**
+   * Deletes a course by ID.
+   * @param id - The ID of the course to delete.
+   * @returns An Observable of void.
+   */
+  deleteCourse(id: number): Observable<void> {
+    return this.coursesEndpoint.delete(id);
+  }
   getCategories(): Observable<Category[]> {
     return this.categoriesEndpoint.getAll();
   }
