440c64bdbba77b3eeb33d64d672c53b5dd41b3dc
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-02 23:24:44 -0500

    fix(environment): remove trailing slash from api base url.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1/',
+  platformProviderApiBaseUrl: 'http://localhost:3000/api/v1',
   platformProviderCategoriesEndpointPath: '/categories',
   platformProviderCoursesEndpointPath: '/courses',
   platformProviderSignInEndpointPath: '/authentication/sign-in',
