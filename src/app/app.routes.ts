399738452eed0b3518f6a4e22f0154b3b5a38db7
Angel Velasquez <pcisavel@upc.edu.pe>
2026-06-22 07:31:31 -0500

    refactor(routes): update route configuration for iam implementation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,19 @@
 import { Routes } from '@angular/router';
 import { Home } from './shared/presentation/views/home/home';
+import { iamGuard } from './iam/infrastructure/iam.guard';
 
 const about = () => import('./shared/presentation/views/about/about').then((m) => m.About);
 const pageNotFound = () =>
   import('./shared/presentation/views/page-not-found/page-not-found').then((m) => m.PageNotFound);
 const learningRoutes = () =>
   import('./learning/presentation/learning.routes').then((m) => m.learningRoutes);
-/*
 // Uncomment when IAM is implemented
 const iamRoutes = () => import('./iam/presentation/iam.routes').then(m => m.iamRoutes);
-*/
 const baseTitle = 'ACME Learning Center';
 
 /**
  * Root route configuration that composes bounded-context routes.
  */
-/*
 // IAM-enabled Routes version to replace when IAM is implemented
 export const routes: Routes = [
   { path: 'home',     component:      Home,                         title: `${baseTitle} - Home`, canActivate: [iamGuard]  },
@@ -25,13 +23,12 @@ export const routes: Routes = [
   { path: '',         redirectTo:     '/home', pathMatch: 'full'  },
   { path: '**',       loadComponent:  pageNotFound,                 title: `${baseTitle} - Page Not Found`  },
 ];
-*/
 
 // Public Routes version to use until IAM is implemented
-export const routes: Routes = [
+/*export const routes: Routes = [
   { path: 'home', component: Home, title: `${baseTitle} - Home` },
   { path: 'about', loadComponent: about, title: `${baseTitle} - About` },
   { path: 'learning', loadChildren: learningRoutes },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', loadComponent: pageNotFound, title: `${baseTitle} - Page Not Found` },
-];
+];*/
